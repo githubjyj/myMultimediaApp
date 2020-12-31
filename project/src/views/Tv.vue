bbcc0381980278787be8b3d8ae37472e590ee4f2
githubjyj <2594131385@qq.com>
2020-12-31 11:02:47 +0800

    Tv

--- project/src/views/Tv.vue
+++ project/src/views/Tv.vue
@@ -19,12 +19,6 @@
 
 <script>
 export default {
-  created() {
-    this.getData();
-  },
-  mounted() {
-    this.listenScroll();
-  },
   data() {
     return {
       tvList: [],
@@ -32,21 +26,23 @@ export default {
       isFinish:true
     };
   },
+  created() {
+    this.getData();
+  },
+  mounted() {
+    this.listenScroll();
+  },
   methods: {
     getData() {
-      console.log("这是第" + this.start +"现在状况"+this.isFinish);
       if (this.isFinish) {
         this.isFinish = false
-        console.log("开始请求前置为"+this.isFinish)
         let baseUrl = "https://bird.ioliu.cn/v2?url=";
         let tvUrl = `https://m.douban.com/rexxar/api/v2/subject_collection/tv_domestic/items?os=ios&for_mobile=1&start=${this.start}&count=10`;
         this.axios
           .get(baseUrl + tvUrl)
           .then((response) => {
-            console.log(response);
             this.tvList = this.tvList.concat(response.data.subject_collection_items);
             this.isFinish = true
-            console.log("请求完毕" + this.isFinish);
           })
           .catch((err) => console.log(err));
       }
@@ -62,7 +58,9 @@ export default {
         fullHeight = htmlDom.offsetHeight;   // 页面总高度
         scrollTop = htmlDom.scrollTop;       // 滚动条距离顶部的距离
         if (deviceHeight + scrollTop > fullHeight - 20) {
-          this.start += 10;
+          if (this.isFinish) {
+            this.start += 10;
+          }
           if (this.start < 50) {
             this.getData();
           }
