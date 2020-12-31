aab17427870882e05788e73ee9c70762602a4c65
githubjyj <2594131385@qq.com>
2020-12-31 10:50:18 +0800

    Tv

--- project/src/views/Tv.vue
+++ project/src/views/Tv.vue
@@ -2,6 +2,7 @@
   <div>
     <ul>
       <li v-for="item in tvList" :key="item.id">
+
         <div class="img">
           <img :src="'https://images.weserv.nl/?url=' + item.cover.url" />
         </div>
@@ -10,6 +11,7 @@
           <h3>{{ item.title }}</h3>
           <p>{{ item.info }}</p>
         </div>
+
       </li>
     </ul>
   </div>
@@ -26,35 +28,44 @@ export default {
   data() {
     return {
       tvList: [],
-      start:0
+      start: 0,
+      isFinish:true
     };
   },
   methods: {
     getData() {
-      let baseUrl = "https://bird.ioliu.cn/v2?url=";
-      let tvUrl =
-        `https://m.douban.com/rexxar/api/v2/subject_collection/tv_domestic/items?os=ios&for_mobile=1&start=${this.start}&count=10`;
-      this.axios
-        .get(baseUrl + tvUrl)
-        .then((response) => {
-          console.log(response);
-          this.tvList = this.tvList.concat(response.data.subject_collection_items);
-        })
-        .catch((err) => console.log(err));
+      console.log("这是第" + this.start +"现在状况"+this.isFinish);
+      if (this.isFinish) {
+        this.isFinish = false
+        console.log("开始请求前置为"+this.isFinish)
+        let baseUrl = "https://bird.ioliu.cn/v2?url=";
+        let tvUrl = `https://m.douban.com/rexxar/api/v2/subject_collection/tv_domestic/items?os=ios&for_mobile=1&start=${this.start}&count=10`;
+        this.axios
+          .get(baseUrl + tvUrl)
+          .then((response) => {
+            console.log(response);
+            this.tvList = this.tvList.concat(response.data.subject_collection_items);
+            this.isFinish = true
+            console.log("请求完毕" + this.isFinish);
+          })
+          .catch((err) => console.log(err));
+      }
     },
+
     listenScroll() {
       let htmlDom = document.documentElement;
       let deviceHeight = 0;
       let fullHeight = 0;
       let scrollTop = 0;
-      window.onscroll =  () => {
+      window.onscroll = () => {
         deviceHeight = htmlDom.clientHeight; // 获取窗口的高度
-        fullHeight = htmlDom.offsetHeight; // 页面总高度
-        scrollTop = htmlDom.scrollTop; // 滚动条距离顶部的距离
-        if (deviceHeight + scrollTop > fullHeight - 20 ) {
-            this.start += 10
-            this.getData()
-            
+        fullHeight = htmlDom.offsetHeight;   // 页面总高度
+        scrollTop = htmlDom.scrollTop;       // 滚动条距离顶部的距离
+        if (deviceHeight + scrollTop > fullHeight - 20) {
+          this.start += 10;
+          if (this.start < 50) {
+            this.getData();
+          }
         }
       };
     },
